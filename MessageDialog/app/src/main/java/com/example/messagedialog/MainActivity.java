c105aca0800d1af6736aa5c577657b50f545a137
smkrevit <smkrevit@gamil.com>
2024-07-29 01:06:21 +0700

    Vidio 13 Android Activity Lifecycle

---
+++ MessageDialog/app/src/main/java/com/example/messagedialog/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.messagedialog;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.sql.SQLOutput;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        System.out.println("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+      System.out.println("onStart");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        System.out.println("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        System.out.println("oStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        System.out.println("onDestroy");
+    }
+}
