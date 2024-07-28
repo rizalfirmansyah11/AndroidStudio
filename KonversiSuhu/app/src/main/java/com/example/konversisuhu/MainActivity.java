3525014f8fce0882a536fc90a476fd810ee33542
smkrevit <smkrevit@gamil.com>
2024-07-29 00:38:05 +0700

    Vidio 11 Projek Konversi Suhu

--- KonversiSuhu/app/src/main/java/com/example/konversisuhu/MainActivity.java
+++ KonversiSuhu/app/src/main/java/com/example/konversisuhu/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
+    EditText etNilai;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +32,49 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         load();
-        isiSpinner();
+
     }
 
     public void load(){
         spinner = findViewById(R.id.spinner);
+        etNilai = findViewById(R.id.etNilai);
+        tvHasil = findViewById(R.id.tvHasil);
     }
 
+    /*
     public void isiSpinner(){
         String[] isi = {"Celcius To Reamur", " Celcius To Fahrenheit", "Celcius To Kelvin"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,isi);
         spinner.setAdapter(adapter);
-    }
+    }*/
 
 
 
     public void btnKonversi(View view) {
         String pilihan = spinner.getSelectedItem().toString();
-        System.out.println(pilihan);
+
+        if (etNilai.getText().toString().equals("")){
+            Toast.makeText(this, "Nilai tidak boleh kosong", Toast.LENGTH_SHORT).show();
+        }else{
+            if (pilihan.equals("Celcius To Reamur")){
+                cToR();
+            }
+            if (pilihan.equals("Celcius To Fahrenheit")){
+                cToF();
+           }
+        }
+
     }
+
+    public void cToR (){
+        double suhu = Double.parseDouble(etNilai.getText().toString());
+        double hasil = (4.0/5.0) * suhu;
+      tvHasil.setText(hasil + "");
+
+    }
+
+    public void cToF(){
+        Toast.makeText(this, "Belum Dibuat", Toast.LENGTH_SHORT).show();
+    }
+
 }
