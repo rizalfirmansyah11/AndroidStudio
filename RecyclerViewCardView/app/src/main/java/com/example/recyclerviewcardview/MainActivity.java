2572961019ff4c0ac00e0af09df1bfc1394b1f8c
smkrevit <smkrevit@gamil.com>
2024-07-30 23:37:46 +0700

    Vidio 22 Android RecyclerView - 3 - Menampilkan Data

--- RecyclerViewCardView/app/src/main/java/com/example/recyclerviewcardview/MainActivity.java
+++ RecyclerViewCardView/app/src/main/java/com/example/recyclerviewcardview/MainActivity.java
@@ -7,8 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SiswaAdapter adapter;
+    List<Siswa> siswaList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        load();
+        isiData();
+    }
+
+    public void load(){
+        recyclerView =findViewById(R.id.rcvSiswa);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+    public void isiData(){
+        siswaList = new ArrayList<Siswa>();
+        siswaList.add(new Siswa("Joni","Surabaya"));
+        siswaList.add(new Siswa("Eko","Surabaya"));
+        siswaList.add(new Siswa("Tejo","Surabaya"));
+        siswaList.add(new Siswa("Siti","Surabaya"));
+        siswaList.add(new Siswa("Roni","Surabaya"));
+        siswaList.add(new Siswa("joni","Surabaya"));
+        siswaList.add(new Siswa("joni","Surabaya"));
+        siswaList.add(new Siswa("joni","Surabaya"));
+        siswaList.add(new Siswa("joni","Surabaya"));
+
+        adapter = new SiswaAdapter(this,siswaList);
+        recyclerView.setAdapter(adapter);
     }
 }
