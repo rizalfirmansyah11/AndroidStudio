de671584d8811dff03ad14ce590df7725350eea9
smkrevit <smkrevit@gamil.com>
2024-07-30 14:14:58 +0700

    Vidio 17 Android DatePicker Dialog

--- DatePicker/app/src/main/java/com/example/datepicker/MainActivity.java
+++ DatePicker/app/src/main/java/com/example/datepicker/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.datepicker;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText etTanggal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        load();
+    }
+
+    public void load(){
+        etTanggal = findViewById(R.id.etTanggal);
+    }
+
+    public void etTanggal(View view) {
+
+        Calendar cal = Calendar.getInstance();
+        int tgl = cal.get(Calendar.DAY_OF_MONTH);
+        int bln = cal.get(Calendar.MONTH);
+        int thn = cal.get(Calendar.YEAR);
+
+        DatePickerDialog dtp = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int thn, int bln, int tgl) {
+                etTanggal.setText(tgl+"-"+bln+"-"+thn);
+            }
+        }, thn, bln, tgl);
+        dtp.show();
     }
 }
